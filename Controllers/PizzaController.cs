6beb9b0b27e9a04a1cffa8a91c7ea74b73605080
RubaCodes <rubacodes@gmail.com>
2022-10-05 14:41:44 +0100

    convertito Actions per chiamate database

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,5 +1,7 @@
-﻿using la_mia_pizzeria_static.Models;
+﻿using la_mia_pizzeria_static.Contexts;
+using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -8,16 +10,22 @@ namespace la_mia_pizzeria_static.Controllers
         public IActionResult Index()
         {
             List<Pizza> menu = new List<Pizza>();
-
-
+            using (PizzaContext db = new PizzaContext())
+            {
+                menu = (from e in db.Pizzas select e).ToList();
+            }
             return View(menu);
         }
 
-        public IActionResult Show(int id) {
-            List<Pizza> menu = new List<Pizza>();
-
-            return View(menu[id]);
-
+        public IActionResult Show(int id)
+        {
+            Pizza pizza = new Pizza();
+            using (PizzaContext db = new PizzaContext())
+            {
+                pizza = db.Pizzas.Where(x => x.PizzaId == id).FirstOrDefault();
+            }
+                return View(pizza);
         }
+
     }
 }
