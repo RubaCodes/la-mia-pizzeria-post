9d7cbc158a95ea1dfe39a5d32d73cba05a1f0227
RubaCodes <rubacodes@gmail.com>
2022-10-05 14:23:56 +0100

    creata tabella pizze

--- Contexts/PizzaContext.cs
+++ Contexts/PizzaContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using la_mia_pizzeria_static.Models;
 namespace la_mia_pizzeria_static.Contexts
 {
-    public class PizzaContext :DbContext
+    public class PizzaContext : DbContext
     {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilding)
+        {
+            optionsBuilding.UseSqlServer("Data Source=localhost;Initial Catalog=db-pizzeria;Integrated Security=True");
+        }
     }
 }
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -2,19 +2,19 @@ namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
-
-        public int PizzaId { get; }
+        public int PizzaId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? ImgPath { get; set; }
-        public decimal? Price { get; set; }
+        public decimal Price { get; set; }
 
-        public Pizza( int id ,string name, string description, string imgpath, decimal price) {
-            Name = name;
-            Description = description;
-            ImgPath = imgpath;
-            Price = price;
-            PizzaId = id;
-        }
+        //public Pizza(int id, string name, string description, string imgpath, decimal price)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    ImgPath = imgpath;
+        //    Price = price;
+        //    PizzaId = id;
+        //}
     }
 }
