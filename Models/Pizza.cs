4a66d4753a044239f7ae9ba63c4375a607bf7f7b
RubaCodes <rubacodes@gmail.com>
2022-10-05 16:19:24 +0100

    alcune correzioni di stile

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -6,15 +6,19 @@ namespace la_mia_pizzeria_static.Models
     public class Pizza
     {
         public int PizzaId { get; set; }
+
         [Required(ErrorMessage = "In nome della pizza e' obbligatorio")]
         [StringLength(50)]
         public string? Name { get; set; }
+
         [Required(ErrorMessage = "La descrizione della pizza e' obbligatoria")]
         [FiveWordsValidation]
         public string? Description { get; set; }
+
         [Required(ErrorMessage = "L'immagine e' obbligatoria")]
         [Url]
         public string? ImgPath { get; set; }
+
         [Required(ErrorMessage = "Il prezzo e' obbligatorio")]
         [Range(1,30, ErrorMessage = "Il prezzo della pizza deve essere incluso tra 1 e 30 euro")]
         public decimal Price { get; set; }
